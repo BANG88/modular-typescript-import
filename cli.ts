5a4261cce10defe7680e092b2697e1f9892fab97
bang <sqibang@gmail.com>
2017-03-30 22:59:50 +0700

    chore: update comments

--- cli.ts
+++ cli.ts
@@ -2,21 +2,15 @@
 import * as meow from 'meow'
 import modularTypescriptImport from './index'
 const cli = meow(`
-	Usage
-	  $ modular-typescript-import --pattern --dist
+Usage
+	$ modular-typescript-import --pattern --dist
 
-	Options
-	  --pattern  glob pattern match your files [Default: src/**/*.@(tsx|ts)]
-	  --dist  the destination will write file to [Default: '']
+Options
+	--pattern  glob pattern match your files [Default: src/**/*.@(tsx|ts)]
+	--dist  the destination will write file to [Default: '']
 
-	Examples
-	  $ modular-typescript-import --pattern test/*.tsx --dist dist
-		❯ tree dist
-			dist
-			└── test
-					└── index.tsx
-
-			1 directory, 1 file
+Examples
+	$ modular-typescript-import --pattern test/*.tsx --dist dist
 `)
 
 const { pattern, dist, ...options } = cli.flags
